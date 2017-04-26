09ac703d90aaddb228295b0eed1f7914325045c9
Mathieu Schroeter <schroeter@epsitec.ch>
2017-04-26 09:27:43 +0200

    Cosmetic: prettify

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var url          = require ('url');
+var url = require ('url');
 var xcraftConfig = require ('xcraft-core-etc') ().load ('xcraft');
-var chestConfig  = require ('xcraft-core-etc') ().load ('xcraft-contrib-chest');
+var chestConfig = require ('xcraft-core-etc') ().load ('xcraft-contrib-chest');
 
 /**
  * Retrieve the real URI behind the URI extensions for zog.
@@ -27,38 +27,44 @@ exports.realUri = function (uri, packageName) {
   var uriObj = url.parse (uri);
 
   switch (uriObj.protocol) {
-  case 'chest:': {
-    var protocol = 'http:';
-    if (parseInt (uriObj.slashes ? uriObj.port : chestConfig.port) === 443) {
-      protocol = 'https:';
+    case 'chest:': {
+      var protocol = 'http:';
+      if (parseInt (uriObj.slashes ? uriObj.port : chestConfig.port) === 443) {
+        protocol = 'https:';
+      }
+
+      var urlHttp = {};
+      urlHttp.protocol = protocol;
+      urlHttp.slashes = true;
+      urlHttp.hostname = uriObj.slashes ? uriObj.hostname : chestConfig.host;
+      urlHttp.port = uriObj.slashes ? uriObj.port : chestConfig.port;
+      urlHttp.pathname = path
+        .join ('/resources/', uriObj.pathname || uriObj.hostname)
+        .replace (/\\/g, '/');
+      return url.format (urlHttp);
     }
 
-    var urlHttp = {};
-    urlHttp.protocol = protocol;
-    urlHttp.slashes  = true;
-    urlHttp.hostname = uriObj.slashes ? uriObj.hostname : chestConfig.host;
-    urlHttp.port     = uriObj.slashes ? uriObj.port     : chestConfig.port;
-    urlHttp.pathname = path.join ('/resources/', uriObj.pathname || uriObj.hostname).replace (/\\/g, '/');
-    return url.format (urlHttp);
-  }
+    case 'self:': {
+      urlFile.protocol = 'file:';
+      urlFile.slashes = true;
+      urlFile.hostname = uriObj.hostname;
+      urlFile.port = uriObj.port;
+      urlFile.pathname = path
+        .join (xcraftConfig.pkgProductsRoot, packageName, uriObj.pathname)
+        .replace (/\\/g, '/');
+      return url.format (urlFile);
+    }
 
-  case 'self:': {
-    urlFile.protocol = 'file:';
-    urlFile.slashes  = true;
-    urlFile.hostname = uriObj.hostname;
-    urlFile.port     = uriObj.port;
-    urlFile.pathname = path.join (xcraftConfig.pkgProductsRoot, packageName, uriObj.pathname).replace (/\\/g, '/');
-    return url.format (urlFile);
-  }
-
-  case 'home:': {
-    urlFile.protocol = 'file:';
-    urlFile.slashes  = true;
-    urlFile.hostname = uriObj.hostname;
-    urlFile.port     = uriObj.port;
-    urlFile.pathname = path.join (xcraftConfig.xcraftRoot, '/home/', uriObj.pathname).replace (/\\/g, '/');
-    return url.format (urlFile);
-  }
+    case 'home:': {
+      urlFile.protocol = 'file:';
+      urlFile.slashes = true;
+      urlFile.hostname = uriObj.hostname;
+      urlFile.port = uriObj.port;
+      urlFile.pathname = path
+        .join (xcraftConfig.xcraftRoot, '/home/', uriObj.pathname)
+        .replace (/\\/g, '/');
+      return url.format (urlFile);
+    }
   }
 
   return uri;
